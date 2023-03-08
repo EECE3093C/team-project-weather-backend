c1e4b721d837a44cfa7f2f62e53359357b96d638
ru55e1l <russell.toney@hotmail.com>
2023-03-08 17:42:04 -0500

    Changed plant table colulm names

--- DatabaseProjectPlantDB/dbo/Tables/Plant.sql
+++ DatabaseProjectPlantDB/dbo/Tables/Plant.sql
@@ -1,9 +1,9 @@
 CREATE TABLE [dbo].[Plant] (
-    [Plant_ID]         BIGINT         IDENTITY (1, 1) NOT NULL,
-    [PlantName]        NVARCHAR (255) NOT NULL,
-    [PlantDescription] VARCHAR (MAX)  NULL,
-    [WeatherType_FK]   INT            NOT NULL,
-    CONSTRAINT [PK_Plant] PRIMARY KEY CLUSTERED ([Plant_ID] ASC)
+    [Id]             BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Name]           NVARCHAR (255) NOT NULL,
+    [Description]    VARCHAR (MAX)  NULL,
+    [WeatherType_FK] INT            NOT NULL,
+    CONSTRAINT [PK_Plant] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
 
